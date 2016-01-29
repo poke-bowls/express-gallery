571ac323de7cd38a6b90c4847150d8bccd58b814
Kent Salcedo <kentsalcedo@gmail.com>
2016-01-28 19:25:04 -1000

    Adds some slight changes to css: changed body background, changed bar opacity and block-content margins

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('default', function() {
+gulp.task('sass', function() {
    gulp.src('sass/styles.scss')
        .pipe(sass().on('error', sass.logError))
        .pipe(gulp.dest('./public/css/'));
 });
+
+gulp.task('watch', function() {
+   gulp.watch('sass/**/*.scss',['sass']);
+});
+
+gulp.task('default', ['sass','watch']);
