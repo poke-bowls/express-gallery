fa4266e375ff5a39ae73bb153d1946a73c32663f
Micah Koki <micah.koki@gmail.com>
2016-02-02 18:30:53 -1000

    login working with static username and password in config file

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ var CONFIG = require('./config/session-config');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-console.log(passport);
-
 app.use(methodOverride(function(req,res){
  var method = req.body._method;
  delete req.body._method;
@@ -25,9 +23,9 @@ app.use(methodOverride(function(req,res){
 app.use(express.static('public'));
 
 var User = db.User;
-var Photo = db.Photo;
+// var Photo = db.Photo;
 
-// app.use('/gallery', gallery);
+app.use('/gallery', gallery);
 
 //Add optional secret key to our session
 app.use(session(CONFIG.SESSION));
@@ -40,46 +38,40 @@ app.use(passport.session());
 
 //in order to maintain persistent login session, the authenticated user must be serialized
 //to the session. The user will be deserialized when each subsequent request is made.
-passport.serializeUser(function (user, done){
-  // user is passed in from Local Strategy
-  // user is attached to req.user
-  return done(null, user);
-});
+// passport.serializeUser(function (user, done){
+//   // user is passed in from Local Strategy
+//   // user is attached to req.user
+//   return done(null, user);
+// });
 
-passport.deserializeUser(function (user, done){
-  return done(null, user);
-});
+// passport.deserializeUser(function (user, done){
+//   return done(null, user);
+// });
 
-passport.use(new LocalStrategy(
-  function (username, password, done) {
-    console.log('hello');
-    var isAuthenticated = authenticate(username, password);
-    if (!isAuthenticated) {// Not authenticated
-      return done(null, false); //No error, but credentials dont match
-    }
+// passport.use(new LocalStrategy(
+//   function (username, password, done) {
+//     var isAuthenticated = authenticate(username, password);
+//     if (!isAuthenticated) {// Not authenticated
+//       return done(null, false); //No error, but credentials dont match
+//     }
 
-    var user = {
-      name : 'test',
-      role : 'ADMIN',
-      color : 'green'
-    };
-    return done(null, user); //Authenticated
-  }
-),console.log('hello2'));
+//     return done(null, user); //Authenticated
+//   }
+// ));
 
 app.set('view engine', 'jade');
 app.set('views', './templates');
 
 // ************************ testing routes ***********
 
-app.get('/gallery', isAuthenticated, function (req, res){
-    Photo.findAll()
-      .then(function(photos){
-        res.render('photos/index', {
-          "Photos" : photos
-        });
-      });
-  });
+// app.get('/gallery', isAuthenticated, function (req, res){
+//     Photo.findAll()
+//       .then(function(photos){
+//         res.render('photos/index', {
+//           "Photos" : photos
+//         });
+//       });
+//   });
 
 // app.get('/gallery/login',function (req, res){
 //     res.render('users/login');
@@ -89,19 +81,15 @@ app.get('/gallery', isAuthenticated, function (req, res){
 //   console.log('post request');
 // });
 
-app.get('/gallery/login', function (req, res){
-    res.render('users/login');
-  });
+// app.get('/gallery/login', function (req, res){
+//     res.render('users/login');
+//   });
 
-app.post('/gallery/login',
-  passport.authenticate('local'),
-  function(req, res) {
-    console.log(req);
-    console.log(res);
-    // If this function gets called, authentication was successful.
-    // `req.user` contains the authenticated user.
-    res.redirect('/users/' + req.user.username);
-  });
+// app.post('/gallery/login',
+//   passport.authenticate('local'),
+//   function(req, res) {
+//     res.redirect('/gallery');
+//   });
 
 // ************** end of testing routes **************
 
@@ -109,27 +97,24 @@ app.post('/gallery/login',
 // middleware to check if user is authenticated **
 // ***********************************************
 
-function authenticate(username, password) {
-  // var CREDENTIALS = CONFIG.CREDENTIALS;
-  // var USERNAME = CREDENTIALS.USERNAME;
-  // var PASSWORD = CREDENTIALS.PASSWORD;
-  // console.log('this is passing through authenticate');
-  // console.log(username);
-  // console.log(USERNAME);
-  // console.log(password);
-  // console.log(PASSWORD);
-  console.log('authenticating');
-  return true;
-  // return (username === USERNAME && password === PASSWORD);
-}
+// function authenticate(username, password) {
+//   var CREDENTIALS = CONFIG.CREDENTIALS;
+//   var USERNAME = CREDENTIALS.USERNAME;
+//   var PASSWORD = CREDENTIALS.PASSWORD;
+//   return (username === USERNAME && password === PASSWORD);
+// }
 
-function isAuthenticated(req, res, next) {
-  console.log('is authenticated');
-  if (!req.isAuthenticated()) {
-    return res.send('error');
-  }
-  return next();
-}
+// function isAuthenticated(req, res, next) {
+//   console.log('is authenticated');
+//   if (!req.isAuthenticated()) {
+//     return res.send('error');
+//   }
+//   return next();
+// }
+
+app.get('/', function (req, res) {
+  res.redirect('/gallery');
+});
 
 app.listen(3000, function() {
   console.log('Connected');
