0ca367d24dd1b0d8ab8a17a3c33695441674ad9c
Micah Koki <micah.koki@gmail.com>
2016-02-02 16:28:56 -1000

    Working on Authentication

--- routes/gallery_routes.js
+++ routes/gallery_routes.js
@@ -1,169 +1,181 @@
-var express = require('express');
-var router = express.Router();
-var bodyParser = require('body-parser');
+// var express = require('express');
+// var router = express.Router();
+// var bodyParser = require('body-parser');
 
-var db = require('./../models');
+// var db = require('./../models');
 
-var User = db.User;
-var Photo = db.Photo;
+// var User = db.User;
+// var Photo = db.Photo;
 
-//authentication
-var passport = require('passport');
-var session = require('express-session');
-var LocalStrategy = require( 'passport-local').Strategy;
-var CONFIG = require('./../config/config.json');
+// //authentication
+// var passport = require('passport');
+// var session = require('express-session');
+// var LocalStrategy = require( 'passport-local').Strategy;
+// var CONFIG = require('./../config/config.json');
 
-router.use(bodyParser.urlencoded({extended: true}));
+// router.use(bodyParser.urlencoded({extended: true}));
 
-router.route('/login')
-  .get(function (req, res){
-    res.render('users/login');
-  })
+// router.route('/login')
+//   .get(function (req, res){
+//     res.render('users/login');
+//   })
 
-  .post(passport.authenticate('local', {
-    successRedirect : '/gallery',
-    failureRedirect : '/'
- }));
+//   .post(passport.authenticate('local', {
 
-router.get('/user', function (req, res){
-  User.findAll()
-    .then(function(users){
-      res.render('users/index', {
-        "Users" : users
-      });
-    });
-});
+//     successRedirect : '/',
+//     failureRedirect : '/gallery/user'
+//  }));
 
-router.post('/users', function (req, res) {
-  User.create({
-    username: req.body.username,
-    password: req.body.password
-  })
-    .then(function (user) {
-      res.redirect('/gallery/user');
-    });
-});
+// router.get('/user', function (req, res){
+//   User.findAll()
+//     .then(function(users){
+//       res.render('users/index', {
+//         "Users" : users
+//       });
+//     });
+// });
 
-router.route('/')
-  .get(function (req, res){
-    Photo.findAll()
-      .then(function(photos){
-        res.render('photos/index', {
-          "Photos" : photos
-        });
-      });
-  })
-  .post(function (req, res) {
-    User.find({
-      where : {
-        username : req.body.author
-      }
-    })
-    .then(function(data){
-      if(data.dataValues.id !== undefined){
-        Photo.create({ author: req.body.author,
-          link : req.body.link,
-          description : req.body.description,
-          UserId : data.dataValues.id })
-          .then(function (gallery) {
-            res.redirect('/gallery');
-          });
-      } else {
-        throw new Error('Invalid author');
-      }
-    })
-    .catch(function(err){
-      res.send( {'success' : false});
-    });
-})
-.put(function (req, res){
-  Photo.findAll()
-    .then(function(photos){
-      res.render('photos/index', {
-        "Photos" : photos
-      });
-    });
-});
+// router.post('/users', function (req, res) {
+//   User.create({
+//     username: req.body.username,
+//     password: req.body.password
+//   })
+//     .then(function (user) {
+//       res.redirect('/gallery/user');
+//     });
+// });
 
-router.get('/new', function (req, res){
-  res.render('photos/new', {
-    "Photos" : req.body
-  });
-});
+// router.route('/')
+//   .get(function (req, res){
+//     Photo.findAll()
+//       .then(function(photos){
+//         res.render('photos/index', {
+//           "Photos" : photos
+//         });
+//       });
+//   })
+//   .post(function (req, res) {
+//     User.find({
+//       where : {
+//         username : req.body.author
+//       }
+//     })
+//     .then(function(data){
+//       if(data.dataValues.id !== undefined){
+//         Photo.create({ author: req.body.author,
+//           link : req.body.link,
+//           description : req.body.description,
+//           UserId : data.dataValues.id })
+//           .then(function (gallery) {
+//             res.redirect('/gallery');
+//           });
+//       } else {
+//         throw new Error('Invalid author');
+//       }
+//     })
+//     .catch(function(err){
+//       res.send( {'success' : false});
+//     });
+// })
+// .put(function (req, res){
+//   Photo.findAll()
+//     .then(function(photos){
+//       res.render('photos/index', {
+//         "Photos" : photos
+//       });
+//     });
+// });
 
-router.route('/:id')
-.get(function (req, res){
-  Photo.find({
-    where : {
-      id : req.params.id
-    }
-  })
-  .then(function(data){
-    Photo.findAll({
-      where : {
-        id : {
-          $ne : req.params.id
-        }
-      }
-    })
-      .then(function(photos){
-          res.render('photos/show', {
-            "otherPhotos" : photos.slice( 0, 5),
-            "photo" : data.dataValues
-          });
-      })
-      .catch(function(err){
-        console.log(err);
-      });
-  })
-  .catch(function(err){
-    res.send({'success' : false});
-  });
-})
-.put(function (req, res){
-  Photo.find({
-    where : {
-      id : req.params.id
-    }
-  })
-  .then(function(data){
-      data.update({
-        link : req.body.link,
-        description : req.body.description });
-      res.redirect('/gallery');
-  })
-  .catch(function(err){
-    res.send( {'success' : false});
-  });
-})
-.delete(function (req, res){
-  Photo.destroy({
-    where : {
-      id : req.params.id
-    }
-  })
-  .then(function(data){
-    res.redirect('/gallery');
-    })
-  .catch(function(err){
-    res.send({"success" : false});
-  });
-});
+// router.get('/new', function (req, res){
+//   res.render('photos/new', {
+//     "Photos" : req.body
+//   });
+// });
 
-router.get('/:id/edit', function (req, res){
-  Photo.find({
-    where : {
-      id : req.params.id
-    }
-  })
-  .then(function(data){
-    res.render( 'photos/edit', {
-      "photo" : data.dataValues
-    });
-  })
-  .catch(function(err){
-    res.send({'success' : false});
-  });
-});
+// router.route('/:id')
+// .get(function (req, res){
+//   Photo.find({
+//     where : {
+//       id : req.params.id
+//     }
+//   })
+//   .then(function(data){
+//     Photo.findAll({
+//       where : {
+//         id : {
+//           $ne : req.params.id
+//         }
+//       }
+//     })
+//       .then(function(photos){
+//           res.render('photos/show', {
+//             "otherPhotos" : photos.slice( 0, 5),
+//             "photo" : data.dataValues
+//           });
+//       })
+//       .catch(function(err){
+//         console.log(err);
+//       });
+//   })
+//   .catch(function(err){
+//     res.send({'success' : false});
+//   });
+// })
+// .put(function (req, res){
+//   Photo.find({
+//     where : {
+//       id : req.params.id
+//     }
+//   })
+//   .then(function(data){
+//       data.update({
+//         link : req.body.link,
+//         description : req.body.description });
+//       res.redirect('/gallery');
+//   })
+//   .catch(function(err){
+//     res.send( {'success' : false});
+//   });
+// })
+// .delete(function (req, res){
+//   Photo.destroy({
+//     where : {
+//       id : req.params.id
+//     }
+//   })
+//   .then(function(data){
+//     res.redirect('/gallery');
+//     })
+//   .catch(function(err){
+//     res.send({"success" : false});
+//   });
+// });
 
-module.exports = router;
+// router.get('/:id/edit', function (req, res){
+//   Photo.find({
+//     where : {
+//       id : req.params.id
+//     }
+//   })
+//   .then(function(data){
+//     res.render( 'photos/edit', {
+//       "photo" : data.dataValues
+//     });
+//   })
+//   .catch(function(err){
+//     res.send({'success' : false});
+//   });
+// });
+
+
+// function authenticate(username, password) {
+//   var CREDENTIALS = CONFIG.CREDENTIALS;
+//   var USERNAME = CREDENTIALS.USERNAME;
+//   var PASSWORD = CREDENTIALS.PASSWORD;
+
+//   return (username === USERNAME && password === PASSWORD);
+// }
+
+
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -12,8 +12,9 @@ var session = require('express-session');
 var LocalStrategy = require( 'passport-local').Strategy;
 var CONFIG = require('./config/session-config');
 
-app.use(bodyParser.urlencoded({extended:true
-}));
+app.use(bodyParser.urlencoded({extended: false}));
+
+console.log(passport);
 
 app.use(methodOverride(function(req,res){
  var method = req.body._method;
@@ -24,8 +25,9 @@ app.use(methodOverride(function(req,res){
 app.use(express.static('public'));
 
 var User = db.User;
+var Photo = db.Photo;
 
-app.use('/gallery', gallery);
+// app.use('/gallery', gallery);
 
 //Add optional secret key to our session
 app.use(session(CONFIG.SESSION));
@@ -48,28 +50,86 @@ passport.deserializeUser(function (user, done){
   return done(null, user);
 });
 
-passport.use( new LocalStrategy(
+passport.use(new LocalStrategy(
   function (username, password, done) {
+    console.log('hello');
     var isAuthenticated = authenticate(username, password);
     if (!isAuthenticated) {// Not authenticated
       return done(null, false); //No error, but credentials dont match
     }
 
     var user = {
-      name : 'Bob',
+      name : 'test',
       role : 'ADMIN',
       color : 'green'
     };
     return done(null, user); //Authenticated
   }
-));
+),console.log('hello2'));
 
 app.set('view engine', 'jade');
 app.set('views', './templates');
 
-app.get('/', function (req, res){
-  res.redirect('/gallery');
-});
+// ************************ testing routes ***********
+
+app.get('/gallery', isAuthenticated, function (req, res){
+    Photo.findAll()
+      .then(function(photos){
+        res.render('photos/index', {
+          "Photos" : photos
+        });
+      });
+  });
+
+// app.get('/gallery/login',function (req, res){
+//     res.render('users/login');
+//   });
+
+// app.post('/gallery/login', function(req,res){
+//   console.log('post request');
+// });
+
+app.get('/gallery/login', function (req, res){
+    res.render('users/login');
+  });
+
+app.post('/gallery/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    console.log(req);
+    console.log(res);
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+  });
+
+// ************** end of testing routes **************
+
+// ***********************************************
+// middleware to check if user is authenticated **
+// ***********************************************
+
+function authenticate(username, password) {
+  // var CREDENTIALS = CONFIG.CREDENTIALS;
+  // var USERNAME = CREDENTIALS.USERNAME;
+  // var PASSWORD = CREDENTIALS.PASSWORD;
+  // console.log('this is passing through authenticate');
+  // console.log(username);
+  // console.log(USERNAME);
+  // console.log(password);
+  // console.log(PASSWORD);
+  console.log('authenticating');
+  return true;
+  // return (username === USERNAME && password === PASSWORD);
+}
+
+function isAuthenticated(req, res, next) {
+  console.log('is authenticated');
+  if (!req.isAuthenticated()) {
+    return res.send('error');
+  }
+  return next();
+}
 
 app.listen(3000, function() {
   console.log('Connected');
